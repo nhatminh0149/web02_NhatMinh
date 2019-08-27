5ed695cc6a9e0eb40f81338c927fa18487de40b3
nhatminh0149 <tranlenhatminh97@gmail.com>
2019-08-27 19:34:41 +0700

    cc

--- salomon/pages/dangky.php
+++ salomon/pages/dangky.php
@@ -1,5 +1,10 @@
 <?php
     require_once __DIR__ .'/../dbconnect.php';
+
+    require_once __DIR__.'/../vendor/autoload.php';
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
 ?>
 
 <form id="dangky" name="dangky" method="post" action="">
@@ -52,5 +57,74 @@
         $sqlInsert = "INSERT INTO khachhang (kh_tendangnhap, kh_matkhau, kh_ten, kh_gioitinh, kh_diachi, kh_dienthoai, kh_email, kh_ngaysinh, kh_thangsinh, kh_namsinh, kh_cmnd, kh_makichhoat, kh_trangthai, kh_quantri) VALUES ('$kh_tendangnhap', '$kh_matkhau', '$kh_ten', $kh_gioitinh, '$kh_diachi', '$kh_dienthoai', '$kh_email', $kh_ngaysinh, $kh_thangsinh, $kh_namsinh, '$kh_cmnd', '$kh_makichhoat', $kh_trangthai, $kh_quantri);";
         //var_dump($sqlInsert); die;
         $resultInsert = mysqli_query($conn, $sqlInsert);
+
+            // Gởi mail kích hoạt tài khoản
+    $mail = new PHPMailer(true);                               // Passing `true` enables exceptions
+    try {
+        //Server settings
+        // http / https (SSL / TLS)
+        // smtp
+        $mail->SMTPDebug = 2;                                  // Enable verbose debug output
+        $mail->isSMTP();                                       // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';                        // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                                // Enable SMTP authentication
+        $mail->Username = 'tranlenhatminh97@gmail.com';// SMTP username
+        $mail->Password = 'tqeizahmkrpxcpyc';                  // SMTP password
+        $mail->SMTPSecure = 'tls';                             // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                     // TCP port to connect to
+        $mail->CharSet = "UTF-8";
+    
+        // Bật chế bộ tự mình mã hóa SSL
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+        //Recipients
+        $mail->setFrom('tranlenhatminh97@gmail.com', '[Hỗ trợ khách hàng] - Thông tin tài khoản!');
+        $mail->addAddress($kh_email);                          // Add a recipient
+        $mail->addReplyTo('minhb1606911@student.ctu.edu.vn', 'Người quản trị Website');
+        // $mail->addCC('cc@example.com');
+        // $mail->addBCC('bcc@example.com');
+    
+        //Attachments
+        // $mail->addAttachment('/var/tmp/file.tar.gz');        // Add attachments
+        // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');   // Optional name
+    
+        //Content
+        $mail->isHTML(true);                                    // Set email format to HTML
+        $mail->Subject = 'Thông báo kích hoạt tài khoản';
+        $siteUrl = 'http://localhost:1000/web02_NhatMinh/salomon/';
+        $body = <<<EOT
+        <table>
+            <tr>
+                <td><h1 style="color: red; font-size: 16px; text-align: center;">TRANG BÁN HÀNG SALOMON</h1>
+                    <img src="https://res.cloudinary.com/drdoqfhly/image/upload/v1530887094/gg-1_synrgy.jpg" width="300px" height="150px" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Xin chào $kh_ten, cám ơn bạn đã đăng ký Hệ thống của chúng tôi. Vui lòng click vào liên kết sau để kích hoạt tài khoản!
+                    <a href="http://localhost:1000/web02_NhatMinh/salomon/pages/kichhoattaikhoan.php?kh_tendangnhap=$kh_tendangnhap&kh_makichhoat=$kh_makichhoat">Kích hoạt tài khoản</a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li>Xem chúng tôi qua <a href="https://facebook.com/fanpagecuaban">Facebook</a></li>
+                        <li>Xem chúng tôi qua <a href="https://twitter.com/fanpagecuaban">Twitter</a></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+EOT;
+        $mail->Body    = $body;
+    
+        $mail->send();
+    } catch (Exception $e) {
+        echo 'Lỗi khi gởi mail: ', $mail->ErrorInfo;
     }
+}
 ?>
---
+++ salomon/pages/kichhoattaikhoan.php
@@ -0,0 +1,17 @@
+<?php
+     require_once __DIR__ .'/../dbconnect.php';
+     $kh_tendangnhap=$_GET['kh_tendangnhap'];
+     $kh_trangthai=$_GET['kh_trangthai'];
+
+     $sqlSelect="SELECT * FROM khachhang WHERE kh_tendangnhap=' $kh_tendangnhapnta' AND kh_trangthai='$kh_trangthai';";
+    $kq=mysqli_query($conn,$sqlSelect);
+    $kqRow=mysqli_fetch_array( $kq, MYSQLI_ASSOC);
+    if(empty($kqRow)) {
+        echo 'Vui lòng kiểm tra lại EMAIL!';
+    } else {
+        // Tìm được dòng khách hàng cần cập nhật
+        $sqlUpdate = "UPDATE khachhang SET kh_trangthai = 1 WHERE kh_tendangnhap = '$kh_tendangnhap'";
+        $resultUpdate = mysqli_query($conn, $sqlUpdate);
+        echo 'Tài khoản đã được kích hoạt. Click vào <a href="http://localhost:1000/web02_NhatMinh/salomon/hinhsanpham/danhsach.php">ĐÂY</a> để đến trang chủ!';
+    }
+?>
