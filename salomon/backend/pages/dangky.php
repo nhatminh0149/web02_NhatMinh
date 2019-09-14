b3b679079abc5f07607e6863825d9e1d53e0105e
nhatminh0149 <tranlenhatminh97@gmail.com>
2019-09-14 19:28:36 +0700

    new

--- salomon/backend/pages/dangky.php
+++ salomon/backend/pages/dangky.php
@@ -1,7 +1,7 @@
 <?php
-    require_once __DIR__ .'/../dbconnect.php';
+    require_once __DIR__ .'/../../dbconnect.php';
 
-    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../../vendor/autoload.php';
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
